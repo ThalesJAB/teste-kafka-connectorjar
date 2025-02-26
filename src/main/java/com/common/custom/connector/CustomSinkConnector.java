c3e7f0f11e3723fb44ac564318d4b4934689e459
Thales Batista <thales.batista-ext@casasbahia.com.br>
2025-02-26 18:37:49 -0300

    wip

--- src/main/java/com/common/custom/connector/CustomSinkConnector.java
+++ src/main/java/com/common/custom/connector/CustomSinkConnector.java
@@ -1,4 +1,44 @@
 package com.common.custom.connector;
 
-public class CustomSinkConnector {
+import com.common.custom.task.CustomSinkTask;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.sink.SinkConnector;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomSinkConnector extends SinkConnector {
+
+    private Map<String, String> configProps;
+
+    @Override
+    public void start(Map<String, String> props) {
+        this.configProps = props;
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return CustomSinkTask.class;  // Define a classe responsável pelo processamento
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        return List.of(configProps);
+    }
+
+    @Override
+    public void stop() {
+        // Código para parar o conector se necessário
+    }
+
+    @Override
+    public ConfigDef config() {
+        return new ConfigDef();
+    }
+
+    @Override
+    public String version() {
+        return "1.0";
+    }
 }
--- src/main/java/com/common/custom/task/CustomSinkTask.java
+++ src/main/java/com/common/custom/task/CustomSinkTask.java
@@ -1,4 +1,86 @@
 package com.common.custom.task;
 
-public class CustomSinkTask {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.common.custom.model.User;
+import com.common.custom.repositories.UserRepository;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.sink.SinkTask;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CustomSinkTask extends SinkTask {
+
+    private UserRepository userRepository;
+
+    @Override
+    public void start(Map<String, String> map) {
+
+    }
+
+    @Override
+    public void put(Collection<SinkRecord> records) {
+        for (SinkRecord record : records) {
+            String topic = record.topic();
+            ObjectMapper objectMapper = new ObjectMapper(); // Cria um ObjectMapper do Jackson
+            Struct valueStruct = (Struct) record.value();
+
+            System.out.println("Received record Struct: " + valueStruct);
+
+
+            try {
+                // Converte o Struct para um Map
+                Map<String, Object> structAsMap = new HashMap<>();
+                for (Field field : valueStruct.schema().fields()) {
+                    String fieldName = field.name();
+                    Object fieldValue = valueStruct.get(field);
+                    structAsMap.put(fieldName, fieldValue);
+                }
+
+                System.out.println("Processed Map: " + structAsMap);
+
+                // Converte o Map para JSON
+                String jsonString = objectMapper.writeValueAsString(structAsMap);
+                System.out.println("Processed JSON String: " + jsonString);
+
+
+                // Se precisar de um JSONObject, você pode criar a partir da string
+                JSONObject json = new JSONObject(jsonString);
+
+                System.out.println("Processed JSONObject: " + json);
+
+                switch (topic) {
+                    case "users-topic":
+                        User user = new User();
+                        user.setName(json.getString("name"));
+                        user.setEmail(json.getString("email"));
+                        userRepository.save(user);
+                        break;
+                    default:
+                        System.out.println("Tópico desconhecido: " + topic);
+                }
+            } catch (Exception e) {
+                System.err.println("Failed to convert Struct to JSON: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public String version() {
+        return "1.0";
+    }
 }
